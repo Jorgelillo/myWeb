2a6ae80bc85982067645185e643e029c8dacd70a
Jorgelillo <jorgelillo@hotmail.com>
2019-02-19 20:19:14 +0100

    Componentes principales de la web

--- myWeb/src/app/app-routing.module.ts
+++ myWeb/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: PrincipalComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PrincipalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myWeb/src/app/app.module.ts
+++ myWeb/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrincipalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
