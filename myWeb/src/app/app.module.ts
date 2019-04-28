036903dc17fea05436088efe6377dfbf5b14f4e9
jorgelillo9 <jorge.duran.corbalan@gmail.com>
2019-04-28 11:15:36 +0200

    Font Awesome + Responsive Navbar

--- myWeb/src/app/app.module.ts
+++ myWeb/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
